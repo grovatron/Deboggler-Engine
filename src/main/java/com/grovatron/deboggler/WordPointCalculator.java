225b7f0003682a14078ea49b1a568491abfa7166
grovatron <grover.a.sundstrom@gmail.com>
2019-04-15 16:26:35 -0400

    Create and document the WordPointCalculator interface.

--- src/main/java/com/grovatron/deboggler/WordPointCalculator.java
+++ src/main/java/com/grovatron/deboggler/WordPointCalculator.java
@@ -1,5 +1,24 @@
 package com.grovatron.deboggler;
 
+import java.util.List;
+
+/**
+ * Determines the point value of <code>Word</code> objects found in the
+ * <code>Letter</code> grid. Different implementations of the interface
+ * allow for different scoring styles (i.e. value determined solely by
+ * word length, adding individual <code>Letter</code> values, etc.).
+ * 
+ * @author Grover Sundstrom (grover.a.sundstrom@gmail.com)
+ * @version 0.0.1 04/15/2019
+ *
+ */
 public interface WordPointCalculator {
 
+	/**
+	 * 
+	 * @param letters A <code>List</code> of <code>Letter</code> objects comprising a
+	 * word found on the grid.
+	 * @return A point value, determined by the implementation of the interface.
+	 */
+	int calculatePoints(List<Letter> letters);
 }
