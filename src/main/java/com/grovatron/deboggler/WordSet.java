7c341168c37e586a5ff496d2517daadbf4e1964e
grovatron <grover.a.sundstrom@gmail.com>
2019-04-15 16:34:40 -0400

    Create and document the WordSet interface.

--- src/main/java/com/grovatron/deboggler/WordSet.java
+++ src/main/java/com/grovatron/deboggler/WordSet.java
@@ -1,5 +1,21 @@
 package com.grovatron.deboggler;
 
+/**
+ * Responsible for collecting <code>Word</code> objects found during the
+ * search process of <code>Deboggler</code>'s <code>getWordList</code>
+ * method, and returning a <code>List<Word></code> at the end of the method.
+ * 
+ * @author Grover Sundstrom (grover.a.sundstrom@gmail.com)
+ * @version 0.0.1 04/19/2019
+ *
+ */
 public interface WordSet {
 
+	/**
+	 * Add a <code>Word</code> to the set of unique, found words.
+	 * @param word <code>Word</code> object to be added.
+	 * @return Boolean value representing whether the <code>Word</code> was
+	 * successfully added.
+	 */
+	boolean addWord(Word word);
 }
