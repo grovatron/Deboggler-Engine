100c048c0fe08b8e11b36493a3f689a423b6753d
grovatron <grover.a.sundstrom@gmail.com>
2019-04-17 13:35:02 -0400

    Create separate tests for all whitespace strings

--- src/test/java/com/grovatron/deboggler/LetterTest.java
+++ src/test/java/com/grovatron/deboggler/LetterTest.java
@@ -25,12 +25,39 @@ class LetterTest {
 	}
 	
 	/**
-	 * Attempt to construct letter with whitespace String, should throw exception.
+	 * Attempt to construct letter with space character String, should throw exception.
 	 */
 	@Test
 	void testConstructor3() {
 		assertThrows(IllegalArgumentException.class, () -> new Letter(" "),
-				"Letter does not take whitespace Strings, should throw IllegalArgumentException");
+				"Letter does not take space character Strings, should throw IllegalArgumentException");
+	}
+	
+	/**
+	 * Attempt to construct letter with tab character String, should throw exception.
+	 */
+	@Test
+	void testConstructor4() {
+		assertThrows(IllegalArgumentException.class, () -> new Letter("\t"),
+				"Letter does not take tab character Strings, should throw IllegalArgumentException");
+	}
+	
+	/**
+	 * Attempt to construct letter with new-line character String, should throw exception.
+	 */
+	@Test
+	void testConstructor5() {
+		assertThrows(IllegalArgumentException.class, () -> new Letter("\n"),
+				"Letter does not take new-line character Strings, should throw IllegalArgumentException");
+	}
+	
+	/**
+	 * Attempt to construct letter with carriage return character String, should throw exception.
+	 */
+	@Test
+	void testConstructor6() {
+		assertThrows(IllegalArgumentException.class, () -> new Letter("\r"),
+				"Letter does not take carriage return character Strings, should throw IllegalArgumentException");
 	}
 
 }
