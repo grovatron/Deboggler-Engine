866b7fd98eef8e1e4d80829e6cd4f2371c92a3d7
grovatron <grover.a.sundstrom@gmail.com>
2019-04-17 13:49:48 -0400

    Rename validateInput to validateStringInput

--- src/main/java/com/grovatron/deboggler/util/LetterValidator.java
+++ src/main/java/com/grovatron/deboggler/util/LetterValidator.java
@@ -1,14 +1,27 @@
 package com.grovatron.deboggler.util;
 
+/**
+ * Responsible for validating constructor arguments for the Letter class.
+ * 
+ * @author Grover Sundstrom (grover.a.sundstrom@gmail.com)
+ * @version 0.0.1 (04/17/2019)
+ *
+ */
 public class LetterValidator {
 
-	public static boolean validateInput(String input) {
+	/**
+	 * Validates Strings for <code>Letter</code>'s <code>letter</code> argument
+	 * in the constructor
+	 * @param input String to be validated
+	 * @return Boolean value representing whether input was valid
+	 */
+	public static boolean validateStringInput(String input) {
 		return input != null &&
 				!input.isEmpty() &&
 				!input.isBlank() &&
 				!containsDigits(input);
 	}
-	
+
 	private static boolean containsDigits(String input) {
 		if (input == null) {
 			return false;
