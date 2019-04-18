6936efcf74f7bc42dda610379777f619f130fd2f
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 12:15:28 -0400

    Create failing test testConstructor6.

--- src/test/java/com/grovatron/deboggler/WordTest.java
+++ src/test/java/com/grovatron/deboggler/WordTest.java
@@ -2,10 +2,13 @@ package com.grovatron.deboggler;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class WordTest {
@@ -74,6 +77,28 @@ class WordTest {
 		List<ValueModifier> modifiers = List.of();
 		assertThrows(IllegalArgumentException.class, () -> new Word(word, value, location, modifiers),
 				"Word does not take empty location list, should throw IllegalArgumentException");
+	}
+	
+	/**
+	 * Attempt to construct Word with location list whose length does not match word length, should throw
+	 * exception.
+	 */
+	@ParameterizedTest
+	@MethodSource("intListProvider")
+	void testConstructor6(List<Integer> locationList) {
+		String word = "APE";
+		int value = 0;
+		List<Integer> location = locationList;
+		List<ValueModifier> modifiers = List.of();
+		assertThrows(IllegalArgumentException.class, () -> new Word(word, value, location, modifiers),
+				"Word does not take location list whose length does not match word length, should throw IllegalArgumentException");
+	}
+	
+	static Stream<List<Integer>> intListProvider() {
+		return Stream.of(
+				Arrays.asList(1,2),
+				Arrays.asList(1, 2, 3, 6)
+		);
 	}
 
 }
