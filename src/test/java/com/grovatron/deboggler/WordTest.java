e8c1219bbd6751f8c8e6154a14288a47d99cca97
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 11:36:14 -0400

    Create failing test testConstructor2.

--- src/test/java/com/grovatron/deboggler/WordTest.java
+++ src/test/java/com/grovatron/deboggler/WordTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class WordTest {
 
 	/**
-	 * Attempt to construct Word will null String, should throw exception.
+	 * Attempt to construct Word with null String, should throw exception.
 	 */
 	@Test
 	void testConstructor1() {
@@ -19,6 +19,19 @@ class WordTest {
 		List<ValueModifier> modifiers = List.of();
 		assertThrows(IllegalArgumentException.class, () -> new Word(word, value, location, modifiers),
 				"Word does not take null Strings, should throw IllegalArgumentException");
+	}
+	
+	/**
+	 * Attempt to construct Word with empty String, should throw exception.
+	 */
+	@Test
+	void testConstructor2() {
+		String word = "";
+		int value = 0;
+		List<Integer> location = List.of(1,2,3);
+		List<ValueModifier> modifiers = List.of();
+		assertThrows(IllegalArgumentException.class, () -> new Word(word, value, location, modifiers),
+				"Word does not take empty Strings, should throw IllegalArgumentException");
 	}
 
 }
