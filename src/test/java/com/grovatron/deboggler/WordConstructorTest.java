8c8ca4c77448208e7e7d9e0da103314ab420d550
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 17:15:06 -0400

    Create failing test testConstructWord2.

--- src/test/java/com/grovatron/deboggler/WordConstructorTest.java
+++ src/test/java/com/grovatron/deboggler/WordConstructorTest.java
@@ -20,13 +20,23 @@ class WordConstructorTest {
 	}
 	
 	/**
-	 * Attempt to call constructWord with null List<Integer>, should throw exception.
+	 * Attempt to call constructWord with null List<Letter>, should throw exception.
 	 */
 	@Test
 	void testConstructWord1() {
 		WordConstructor wordConstructor = new WordConstructor(new OriginalPointCalculator());
 		assertThrows(IllegalArgumentException.class, () -> wordConstructor.constructWord(null, List.of(1,2,3)),
-				"constructWord does not take null List<Integer>, should throw IllegalArgumentException");
+				"constructWord does not take null List<Letter>, should throw IllegalArgumentException");
+	}
+	
+	/**
+	 * Attempt to call constructWord with empty List<Letter>, should throw exception.
+	 */
+	@Test
+	void testConstructWord2() {
+		WordConstructor wordConstructor = new WordConstructor(new OriginalPointCalculator());
+		assertThrows(IllegalArgumentException.class, () -> wordConstructor.constructWord(List.of(), List.of(1,2,3)),
+				"constructWord does not take empty List<Integer>, should throw IllegalArgumentException");
 	}
 
 }
