95b375415a10171e38ba329b5647061725814513
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 13:31:15 -0400

    Create constructor for WordConstructor

--- src/main/java/com/grovatron/deboggler/WordConstructor.java
+++ src/main/java/com/grovatron/deboggler/WordConstructor.java
@@ -1,5 +1,22 @@
 package com.grovatron.deboggler;
 
+/**
+ * Responsible for constructing words found on the 2d <code>Letter</code> matrix.
+ * 
+ * @author Grover Sundstrom
+ * @version 0.0.1 (04/18/2019)
+ *
+ */
 public class WordConstructor {
 
+	private final WordPointCalculator wordPointCalculator;
+
+	/**
+	 * Creates an instance of the <code>WordConstructor</code>
+	 * @param wordPointCalculator An object that implements the <code>WordPointCalculator</code>
+	 * interface.
+	 */
+	public WordConstructor(WordPointCalculator wordPointCalculator) {
+		this.wordPointCalculator = wordPointCalculator;
+	}
 }
