f3cd5646d843e714025e6fa37a45b89dc9b253c5
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 16:56:01 -0400

    Create WordConstructorTest class.

---
+++ src/test/java/com/grovatron/deboggler/WordConstructorTest.java
@@ -0,0 +1,14 @@
+package com.grovatron.deboggler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WordConstructorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
