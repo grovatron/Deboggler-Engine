12c5965718530b8a027f55d97aa544dd5b05fbe0
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 17:06:16 -0400

    Create failing test testConstructWord1.

--- src/test/java/com/grovatron/deboggler/WordConstructorTest.java
+++ src/test/java/com/grovatron/deboggler/WordConstructorTest.java
@@ -2,6 +2,8 @@ package com.grovatron.deboggler;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class WordConstructorTest {
@@ -15,6 +17,16 @@ class WordConstructorTest {
 		assertThrows(IllegalArgumentException.class, () -> new WordConstructor(null),
 				"WordConstructor does not take null WordPointCalculator, should throw"
 				+ " IllegalArgumentException.");
+	}
+	
+	/**
+	 * Attempt to call constructWord with null List<Integer>, should throw exception.
+	 */
+	@Test
+	void testConstructWord1() {
+		WordConstructor wordConstructor = new WordConstructor(new OriginalPointCalculator());
+		assertThrows(IllegalArgumentException.class, () -> wordConstructor.constructWord(null, List.of(1,2,3)),
+				"constructWord does not take null List<Integer>, should throw IllegalArgumentException");
 	}
 
 }
