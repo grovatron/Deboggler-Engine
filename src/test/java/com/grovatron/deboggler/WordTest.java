0ce725c8ccf70f8cc967631351435183d83f58ad
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 11:42:11 -0400

    Create (passing) test testConstructor3

--- src/test/java/com/grovatron/deboggler/WordTest.java
+++ src/test/java/com/grovatron/deboggler/WordTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class WordTest {
 
@@ -32,6 +34,20 @@ class WordTest {
 		List<ValueModifier> modifiers = List.of();
 		assertThrows(IllegalArgumentException.class, () -> new Word(word, value, location, modifiers),
 				"Word does not take empty Strings, should throw IllegalArgumentException");
+	}
+	
+	/**
+	 * Attempt to construct Word with whitespace String, should throw exception.
+	 */
+	@ParameterizedTest
+	@ValueSource(strings = {" ", "\t", "\n", "\r"})
+	void testConstructor3(String whitespace) {
+		String word = whitespace;
+		int value = 0;
+		List<Integer> location = List.of(1,2,3);
+		List<ValueModifier> modifiers = List.of();
+		assertThrows(IllegalArgumentException.class, () -> new Word(word, value, location, modifiers),
+				"Word does not take whitespace Strings, should throw IllegalArgumentException");
 	}
 
 }
