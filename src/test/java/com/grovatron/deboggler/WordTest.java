ebcfef2458a07b431e8ee83dae8effe2987d4cbb
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 11:34:31 -0400

    Create failing test testConstructor1

--- src/test/java/com/grovatron/deboggler/WordTest.java
+++ src/test/java/com/grovatron/deboggler/WordTest.java
@@ -2,13 +2,23 @@ package com.grovatron.deboggler;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class WordTest {
 
+	/**
+	 * Attempt to construct Word will null String, should throw exception.
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstructor1() {
+		String word = null;
+		int value = 0;
+		List<Integer> location = List.of(1,2,3);
+		List<ValueModifier> modifiers = List.of();
+		assertThrows(IllegalArgumentException.class, () -> new Word(word, value, location, modifiers),
+				"Word does not take null Strings, should throw IllegalArgumentException");
 	}
 
 }
