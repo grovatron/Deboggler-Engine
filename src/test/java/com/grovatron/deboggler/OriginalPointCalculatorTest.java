26c3e6e68bb4d9ee41aac21a3800d8f489d418b0
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 15:50:05 -0400

    Create failing test testCalculatePoints1.

--- src/test/java/com/grovatron/deboggler/OriginalPointCalculatorTest.java
+++ src/test/java/com/grovatron/deboggler/OriginalPointCalculatorTest.java
@@ -2,13 +2,26 @@ package com.grovatron.deboggler;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class OriginalPointCalculatorTest {
+	
+	WordPointCalculator wordPointCalculator;
+	
+	@BeforeEach
+	void setup() {
+		wordPointCalculator = new OriginalPointCalculator();
+	}
 
+	/**
+	 * Attempt to pass null List<Letter> to calculatePoints method, should
+	 * throw exception.
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCalculatePoints1() {
+		assertThrows(IllegalArgumentException.class, () -> wordPointCalculator.calculatePoints(null),
+				"calculatePoints does not take null List<Letter>, should throw IllegalArgumentException.");
 	}
 
 }
