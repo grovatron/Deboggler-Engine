32ac1c4aef4152c9a9c719a7c4a09b01cef7d0a6
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 15:58:53 -0400

    Get OriginalPointCalculator to pass failing test.

--- src/main/java/com/grovatron/deboggler/OriginalPointCalculator.java
+++ src/main/java/com/grovatron/deboggler/OriginalPointCalculator.java
@@ -6,7 +6,7 @@ public class OriginalPointCalculator implements WordPointCalculator {
 
 	@Override
 	public int calculatePoints(List<Letter> letters) {
-		if (letters == null || letters.isEmpty()) {
+		if (letters == null || letters.isEmpty() || letters.contains(null)) {
 			throw new IllegalArgumentException();
 		}
 		// TODO Auto-generated method stub
