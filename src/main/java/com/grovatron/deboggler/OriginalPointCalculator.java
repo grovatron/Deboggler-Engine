9fe37d73a39cc94e10e09fcba5de9333265b30d0
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 14:35:27 -0400

    Create OriginalPointCalculator class

---
+++ src/main/java/com/grovatron/deboggler/OriginalPointCalculator.java
@@ -0,0 +1,13 @@
+package com.grovatron.deboggler;
+
+import java.util.List;
+
+public class OriginalPointCalculator implements WordPointCalculator {
+
+	@Override
+	public int calculatePoints(List<Letter> letters) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
