364584bd66d8f740e3d2b9104703ed5c4fd31c8a
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 21:31:01 -0400

    Create failing test testAddWord1.

--- src/test/java/com/grovatron/deboggler/HashMapWordSetTest.java
+++ src/test/java/com/grovatron/deboggler/HashMapWordSetTest.java
@@ -2,6 +2,9 @@ package com.grovatron.deboggler;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +15,22 @@ class HashMapWordSetTest {
 	@BeforeEach
 	void setup() {
 		wordSet = new HashMapWordSet();
+	}
+	
+	/**
+	 * Add word to HashMapWordSet, should return true.
+	 */
+	@Test
+	void testAddWord1() {
+		String wordString = "APE";
+		int value = 1;
+		List<Integer> location = Arrays.asList(1, 2, 3);
+		List<ValueModifier> modifiers = Arrays.asList();
+		Word word = new Word(wordString, value, location, modifiers);
+		
+		boolean expected = true;
+		boolean actual = wordSet.addWord(word);
+		assertEquals(expected, actual, "Should return true");
 	}
 
 }
