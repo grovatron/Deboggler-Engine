18edbba7e4e5bd782acbc4e5f196c260813447e3
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 05:49:15 -0400

    Create TrieDictionary class.

---
+++ src/main/java/com/grovatron/deboggler/dictionary/TrieDictionary.java
@@ -0,0 +1,17 @@
+package com.grovatron.deboggler.dictionary;
+
+public class TrieDictionary implements Dictionary {
+
+	@Override
+	public boolean isValidPrefix(String prefix) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isValidWord(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
