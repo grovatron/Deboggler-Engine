e4e4f1cd03cb612381334a986579ef59b439cefd
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 13:57:14 -0400

    Create failing test testGetWordList5.

--- src/test/java/com/grovatron/deboggler/DebogglerTest.java
+++ src/test/java/com/grovatron/deboggler/DebogglerTest.java
@@ -6,6 +6,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -106,6 +107,26 @@ class DebogglerTest {
 		assertThrows(IllegalArgumentException.class, () -> deboggler.getWordList(letterGrid),
 				"getWordList does not take Letter[][] with less than 2 rows and columns, "
 				+ "should throw IllegalArgumentException.");
+	}
+	
+	/**
+	 * Pass valid Letter[][], should return list of all possible words
+	 */
+	@Test
+	void testGetWordList5() {
+		Letter[][] letterGrid = {new Letter[] {new Letter("S"), new Letter("T")}, new Letter[] {new Letter("A"), new Letter("R")}};
+		Word word1 = new Word("STAR", 1, List.of(0,1,2,3), List.of());
+		Word word2 = new Word("ARTS", 1, List.of(2,3,1,0), List.of());
+		Word word3 = new Word("TARS", 1, List.of(1,2,3,0), List.of());
+		Word word4 = new Word("RATS", 1, List.of(3,2,1,0), List.of());
+		Word word5 = new Word("ART", 1, List.of(2,3,1), List.of());
+		Word word6 = new Word("TAR", 1, List.of(1,2,3), List.of());
+		Word word7 = new Word("RAT", 1, List.of(3,2,1), List.of());
+		List<Word> wordList = deboggler.getWordList(letterGrid);
+		boolean expected = true;
+		boolean actual = wordList.contains(word1) && wordList.contains(word2) && wordList.contains(word3) && wordList.contains(word4)
+				&& wordList.contains(word5) && wordList.contains(word6) && wordList.contains(word7);
+		assertEquals(expected, actual, "List should contain all seven words");
 	}
 
 }
