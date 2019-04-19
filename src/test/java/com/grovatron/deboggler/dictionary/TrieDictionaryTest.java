836206ef3a41dc1c526a7ab8c6516d7cc243d34d
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 07:51:47 -0400

    Create TrieDictionaryTest class.

---
+++ src/test/java/com/grovatron/deboggler/dictionary/TrieDictionaryTest.java
@@ -0,0 +1,14 @@
+package com.grovatron.deboggler.dictionary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TrieDictionaryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
