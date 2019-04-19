ccfea0785e6b0ab585a1c802e2d9d3e12fe0205c
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 08:07:35 -0400

    Create TrieDictionaryConstructor

---
+++ src/main/java/com/grovatron/deboggler/dictionary/TrieDictionaryConstructor.java
@@ -0,0 +1,22 @@
+package com.grovatron.deboggler.dictionary;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Responsible for constructing a <code>TrieDictionary</code> from an
+ * <code>InputStream</code> of words.
+ * 
+ * @author Grover Sundstrom
+ * @version 0.0.1 04/19/2019
+ *
+ */
+public class TrieDictionaryConstructor implements DictionaryConstructor {
+
+	@Override
+	public Dictionary buildDictionary(InputStream inputStream) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
