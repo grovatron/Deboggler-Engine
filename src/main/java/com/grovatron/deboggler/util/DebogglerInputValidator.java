5ba7c64b81d654a0e9e29cd9a69d42e5f060a100
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 12:09:06 -0400

    Add validateConstructorArguments method to DebogglerInputValidator.

--- src/main/java/com/grovatron/deboggler/util/DebogglerInputValidator.java
+++ src/main/java/com/grovatron/deboggler/util/DebogglerInputValidator.java
@@ -1,16 +1,50 @@
 package com.grovatron.deboggler.util;
 
 import com.grovatron.deboggler.Letter;
+import com.grovatron.deboggler.WordConstructor;
+import com.grovatron.deboggler.WordSet;
+import com.grovatron.deboggler.dictionary.Dictionary;
 
 /**
- * Responsible for validating the <code>Letter[][] letterGrid</code> argument in
- * <code>Deboggler</code>'s <code>getWordList</code> method.
+ * Responsible for validating all input for the <code>Deboggler</code> class.
  * 
  * @author Grover Sundstrom (grover.a.sundstrom@gmail.com)
  * @version 0.0.1 04/19/2019
  *
  */
 public class DebogglerInputValidator {
+	
+	public static void validateConstructorArguments(Dictionary dictionary, WordConstructor wordConstructor,
+			WordSet wordSet, int minLength) {
+		checkIfDictonaryNull(dictionary);
+		checkIfWordConstructorNull(wordConstructor);
+		checkIfWordSetNull(wordSet);
+		checkIfMinLengthGreaterThanOne(minLength);
+	}
+	
+	private static void checkIfDictonaryNull(Dictionary dictionary) {
+		if (dictionary == null) {
+			throw new IllegalArgumentException("Constructor does not take null Dictionary value");
+		}
+	}
+	
+	private static void checkIfWordConstructorNull(WordConstructor wordConstructor) {
+		if (wordConstructor == null) {
+			throw new IllegalArgumentException("Constructor does not take null WordConstructor value");
+		}
+	}
+	
+	private static void checkIfWordSetNull(WordSet wordSet) {
+		if (wordSet == null) {
+			throw new IllegalArgumentException("Constructor does not take null WordSet value");
+		}
+	}
+	
+	private static void checkIfMinLengthGreaterThanOne(int minLength) {
+		if (minLength < 1) {
+			throw new IllegalArgumentException("Constructor does not take minLength < 1");
+		}
+	}
 
 	/**
 	 * Validates <code>Letter[][] letterGrid</code> argument.
