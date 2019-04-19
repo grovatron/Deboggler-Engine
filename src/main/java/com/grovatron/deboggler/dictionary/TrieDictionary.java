ef2aa34a46adcaccf2ddd8a5c9d811fc65677f0a
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 05:52:32 -0400

    Add class level javadoc comment.

--- src/main/java/com/grovatron/deboggler/dictionary/TrieDictionary.java
+++ src/main/java/com/grovatron/deboggler/dictionary/TrieDictionary.java
@@ -1,13 +1,23 @@
 package com.grovatron.deboggler.dictionary;
 
+/**
+ * Responsible for validating prefixes and words found in the search process
+ * of <code>Deboggler</code>'s <code>getWordList</code> method.
+ * 
+ * @author Grover Sundstrom (grover.a.sundstrom@gmail.com)
+ * @version 0.0.1 04/19/2019
+ *
+ */
 public class TrieDictionary implements Dictionary {
 
+	
 	@Override
 	public boolean isValidPrefix(String prefix) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	
 	@Override
 	public boolean isValidWord(String word) {
 		// TODO Auto-generated method stub
