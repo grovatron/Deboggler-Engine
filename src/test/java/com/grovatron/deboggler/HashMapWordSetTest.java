812f6bb413cba4119ca46352c0753621b88f9b67
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 21:35:06 -0400

    Create failing test testAddWord2.

--- src/test/java/com/grovatron/deboggler/HashMapWordSetTest.java
+++ src/test/java/com/grovatron/deboggler/HashMapWordSetTest.java
@@ -32,5 +32,22 @@ class HashMapWordSetTest {
 		boolean actual = wordSet.addWord(word);
 		assertEquals(expected, actual, "Should return true");
 	}
+	
+	/**
+	 * Add the same word (same String and int value) twice to HashMapWordSet, should return false.
+	 */
+	@Test
+	void testAddWord2() {
+		String wordString = "APE";
+		int value = 1;
+		List<Integer> location = Arrays.asList(1, 2, 3);
+		List<ValueModifier> modifiers = Arrays.asList();
+		Word word = new Word(wordString, value, location, modifiers);
+		
+		boolean expected = false;
+		wordSet.addWord(word);
+		boolean actual = wordSet.addWord(word);
+		assertEquals(expected, actual, "Should return false");
+	}
 
 }
