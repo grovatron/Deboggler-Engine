3a90f78f96f027a0f90e6dd7db51a51bcc10b872
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 08:32:49 -0400

    Create failing test testBuildDictionary2.

--- src/test/java/com/grovatron/deboggler/dictionary/TrieDictionaryConstructorTest.java
+++ src/test/java/com/grovatron/deboggler/dictionary/TrieDictionaryConstructorTest.java
@@ -2,6 +2,8 @@ package com.grovatron.deboggler.dictionary;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.InputStream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +23,16 @@ class TrieDictionaryConstructorTest {
 	void testBuildDictionary1() {
 		assertThrows(IllegalArgumentException.class, () -> dictionaryConstructor.buildDictionary(null),
 				"buildDictionary does not take null InputStream, should throw IllegalArgumentException.");
+	}
+	
+	/**
+	 * Attempt to pass InputStream that contains no words to buildDictionary, should throw exception.
+	 */
+	@Test
+	void testBuildDictionary2() {
+		InputStream inputStream = this.getClass().getResourceAsStream("/empty-file.txt");
+		assertThrows(IllegalArgumentException.class, () -> dictionaryConstructor.buildDictionary(inputStream),
+				"InputStream must contain at least one word, should throw IllegalArgumentException");
 	}
 
 }
