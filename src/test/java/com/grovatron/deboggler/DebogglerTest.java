a6ee48d5b6a86032f652dcc0a916b409a5dece49
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 11:14:01 -0400

    Create setup method.

--- src/test/java/com/grovatron/deboggler/DebogglerTest.java
+++ src/test/java/com/grovatron/deboggler/DebogglerTest.java
@@ -2,10 +2,35 @@ package com.grovatron.deboggler;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class DebogglerTest {
+import com.grovatron.deboggler.dictionary.Dictionary;
+import com.grovatron.deboggler.dictionary.TrieDictionaryConstructor;
 
+class DebogglerTest {
+	
+	static String testFile = "star\narts\nrats\ntars\nart\ntar\nrat";
+
+	Dictionary dictionary;
+	WordConstructor wordConstructor;
+	WordSet wordSet;
+	int minLength;
+	
+	@BeforeEach
+	void setup() throws IOException {
+		InputStream inputStream = new ByteArrayInputStream(testFile.getBytes(Charset.forName("UTF-8")));
+		dictionary = new TrieDictionaryConstructor().buildDictionary(inputStream);
+		wordConstructor = new WordConstructor(new OriginalPointCalculator());
+		wordSet = new HashMapWordSet();
+		minLength = 3;
+	}
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
