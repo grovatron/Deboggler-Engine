98526023959ac16d85cbead606b4e6007b782645
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 21:24:09 -0400

    Create HashMapWordSetTest class.

---
+++ src/test/java/com/grovatron/deboggler/HashMapWordSetTest.java
@@ -0,0 +1,14 @@
+package com.grovatron.deboggler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HashMapWordSetTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
