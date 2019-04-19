9e55324d9da46d963da26a7086b5ac9a91ac6401
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 08:14:30 -0400

    Create failing test testBuildDictionary1.

--- src/test/java/com/grovatron/deboggler/dictionary/TrieDictionaryConstructorTest.java
+++ src/test/java/com/grovatron/deboggler/dictionary/TrieDictionaryConstructorTest.java
@@ -2,13 +2,25 @@ package com.grovatron.deboggler.dictionary;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TrieDictionaryConstructorTest {
+	
+	DictionaryConstructor dictionaryConstructor;
+	
+	@BeforeEach
+	void setup() {
+		dictionaryConstructor = new TrieDictionaryConstructor();
+	}
 
+	/**
+	 * Attempt to pass null InputStream to buildDictionary, should throw exception.
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testBuildDictionary1() {
+		assertThrows(IllegalArgumentException.class, () -> dictionaryConstructor.buildDictionary(null),
+				"buildDictionary does not take null InputStream, should throw IllegalArgumentException.");
 	}
 
 }
