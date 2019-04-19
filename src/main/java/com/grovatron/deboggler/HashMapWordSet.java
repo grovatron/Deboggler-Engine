beba3d949f70fa9160844ae1c9d2b445e2139b69
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 21:22:24 -0400

    Add javadoc comments to HashMapWordSet class.

--- src/main/java/com/grovatron/deboggler/HashMapWordSet.java
+++ src/main/java/com/grovatron/deboggler/HashMapWordSet.java
@@ -1,15 +1,46 @@
 package com.grovatron.deboggler;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+/**
+ * Responsible for storing <code>Word</code> objects found while searching
+ * <code>Letter</code> grid and returning <code>Word</code> list when search is
+ * complete.
+ * 
+ * @author Grover Sundstrom (grover.a.sundstrom@gmail.com)
+ * @version 0.0.1 04/18/2019
+ *
+ */
 public class HashMapWordSet implements WordSet {
+	
+	private Map<String, Word> wordMap;
+	
+	/**
+	 * Creates an instance of HashMapWordSet.
+	 */
+	public HashMapWordSet() {
+		wordMap = new HashMap<>();
+	}
 
+	/**
+	 * Adds a <code>Word</code> to the <code>HashMapWordSet</code>.
+	 * @param word <code>Word</code> to be added to the <code>HashMapWordSet</code>
+	 * @return Boolean value representing whether the <code>Word</code> was
+	 * successfully added.
+	 */
 	@Override
 	public boolean addWord(Word word) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Returns a list of all the <code>Word</code> objects in the
+	 * <code>HashMapWordSet</code>
+	 * @return List of <code>Word</code> objects.
+	 */
 	@Override
 	public List<Word> getWords() {
 		// TODO Auto-generated method stub
