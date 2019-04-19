840aa937a4db7de3e911f3e0b6385f926f3f146c
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 08:02:27 -0400

    Add javadoc comments to DictionaryConstructor.

--- src/test/java/com/grovatron/deboggler/dictionary/DictionaryConstructor.java
+++ src/test/java/com/grovatron/deboggler/dictionary/DictionaryConstructor.java
@@ -2,7 +2,22 @@ package com.grovatron.deboggler.dictionary;
 
 import java.io.InputStream;
 
+/**
+ * Responsible for reading in words from an <code>InputStream</code> and
+ * constructing an object that implements the <code>Dictionary</code>
+ * interface.
+ * 
+ * @author Grover Sundstrom (grover.a.sundstrom@gmail.com)
+ * @version 0.0.1 04/19/2019
+ *
+ */
 public interface DictionaryConstructor {
 
+	/**
+	 * Builds an object that implements the <code>Dictionary</code>
+	 * interface.
+	 * @param inputStream Stream of words (one per line)
+	 * @return A fully constructed object that implements the <code>Dictionary</code> interface.
+	 */
 	Dictionary buildDictionary(InputStream inputStream);
 }
