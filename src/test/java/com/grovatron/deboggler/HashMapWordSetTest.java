2edee3a7afb20f67dcc3b4967c0df413aab3e534
grovatron <grover.a.sundstrom@gmail.com>
2019-04-19 05:30:19 -0400

    Create passing test testGetWords3.

--- src/test/java/com/grovatron/deboggler/HashMapWordSetTest.java
+++ src/test/java/com/grovatron/deboggler/HashMapWordSetTest.java
@@ -146,5 +146,64 @@ class HashMapWordSetTest {
 		
 		assertTrue(containsWord1 && containsWord2 && containsWord3 && isSize3, "Should be true.");
 	}
+	
+	/**
+	 * Call getWords on HashMapWordSet that has non-unique words added to it, expect List<Word>
+	 * contains only the words with higher values.
+	 */
+	@Test
+	void testGetWords3() {
+		String wordString1 = "APE";
+		int value1 = 1;
+		List<Integer> location1 = Arrays.asList(1, 2, 3);
+		List<ValueModifier> modifiers1 = Arrays.asList();
+		Word word1 = new Word(wordString1, value1, location1, modifiers1);
+		
+		String wordString2 = "APE";
+		int value2 = 5;
+		List<Integer> location2 = Arrays.asList(1, 2, 3);
+		List<ValueModifier> modifiers2 = Arrays.asList();
+		Word word2 = new Word(wordString2, value2, location2, modifiers2);
+		
+		String wordString3 = "TULIP";
+		int value3 = 3;
+		List<Integer> location3 = Arrays.asList(12, 13, 14, 15, 11);
+		List<ValueModifier> modifiers3 = Arrays.asList();
+		Word word3 = new Word(wordString3, value3, location3, modifiers3);
+		
+		String wordString4 = "TULIP";
+		int value4 = 1;
+		List<Integer> location4 = Arrays.asList(12, 13, 14, 15, 11);
+		List<ValueModifier> modifiers4 = Arrays.asList();
+		Word word4 = new Word(wordString4, value4, location4, modifiers4);
+		
+		String wordString5 = "QUESTION";
+		int value5 = 11;
+		List<Integer> location5 = Arrays.asList(0, 4, 5, 6, 3, 7, 9);
+		List<ValueModifier> modifiers5 = Arrays.asList();
+		Word word5 = new Word(wordString5, value5, location5, modifiers5);
+		
+		String wordString6 = "QUESTION";
+		int value6 = 12;
+		List<Integer> location6 = Arrays.asList(0, 4, 5, 6, 3, 7, 9);
+		List<ValueModifier> modifiers6 = Arrays.asList();
+		Word word6 = new Word(wordString6, value6, location6, modifiers6);
+		
+		wordSet.addWord(word1);
+		wordSet.addWord(word2);
+		wordSet.addWord(word3);
+		wordSet.addWord(word4);
+		wordSet.addWord(word5);
+		wordSet.addWord(word6);
+		
+		List<Word> wordList = wordSet.getWords();
+		
+		boolean containsWord1 = wordList.contains(word2);
+		boolean containsWord2 = wordList.contains(word3);
+		boolean containsWord3 = wordList.contains(word6);
+		boolean isSize3 = wordList.size() == 3;
+		
+		assertTrue(containsWord1 && containsWord2 && containsWord3 && isSize3, "Should be true.");
+	}
 
 }
