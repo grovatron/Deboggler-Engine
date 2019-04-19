a193d03ea02ac49f5d8bd87ed6243b3c92be3d26
grovatron <grover.a.sundstrom@gmail.com>
2019-04-18 21:15:43 -0400

    Create HashMapWordSet class

---
+++ src/main/java/com/grovatron/deboggler/HashMapWordSet.java
@@ -0,0 +1,19 @@
+package com.grovatron.deboggler;
+
+import java.util.List;
+
+public class HashMapWordSet implements WordSet {
+
+	@Override
+	public boolean addWord(Word word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Word> getWords() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
