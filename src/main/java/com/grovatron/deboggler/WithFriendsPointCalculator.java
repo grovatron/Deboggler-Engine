0e960491555ca25c9ad55f464bfd501c52e4841f
grovatron <grover.a.sundstrom@gmail.com>
2019-04-20 07:09:50 -0400

    Get WithFriendsPointCalculator to pass failing test.

--- src/main/java/com/grovatron/deboggler/WithFriendsPointCalculator.java
+++ src/main/java/com/grovatron/deboggler/WithFriendsPointCalculator.java
@@ -6,6 +6,9 @@ public class WithFriendsPointCalculator implements WordPointCalculator {
 
 	@Override
 	public int calculatePoints(List<Letter> letters) {
+		if (letters == null) {
+			throw new IllegalArgumentException("calculatePoints does not take null List<Letter> argument.");
+		}
 		// TODO Auto-generated method stub
 		return 0;
 	}
