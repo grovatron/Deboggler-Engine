85a1a111e62d84b1d67848123759b817232e8c7e
grovatron <grover.a.sundstrom@gmail.com>
2019-04-20 06:58:41 -0400

    Create WithFriendsPointCalculator class.

---
+++ src/main/java/com/grovatron/deboggler/WithFriendsPointCalculator.java
@@ -0,0 +1,13 @@
+package com.grovatron.deboggler;
+
+import java.util.List;
+
+public class WithFriendsPointCalculator implements WordPointCalculator {
+
+	@Override
+	public int calculatePoints(List<Letter> letters) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
