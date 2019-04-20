6f6c51c417b85dbf0dae5b7e8015d535ea4d8649
grovatron <grover.a.sundstrom@gmail.com>
2019-04-20 07:17:16 -0400

    Create failing test testCalculatePoints3.

--- src/test/java/com/grovatron/deboggler/WithFriendsPointCalculatorTest.java
+++ src/test/java/com/grovatron/deboggler/WithFriendsPointCalculatorTest.java
@@ -3,6 +3,7 @@ package com.grovatron.deboggler;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,6 +33,17 @@ class WithFriendsPointCalculatorTest {
 	void testCalculatePoints2() {
 		assertThrows(IllegalArgumentException.class, () -> calculator.calculatePoints(Arrays.asList()),
 				"calculatePoints does not take empty List<Letter>, should throw IllegalArgumentException.");
+	}
+	
+	/**
+	 * Pass List<Letter> that contains null values to calculatePoints, should throw exception.
+	 */
+	@Test
+	void testCalculatePoints3() {
+		List<Letter> letters = Arrays.asList(new Letter("A"), null, new Letter("P"));
+		assertThrows(IllegalArgumentException.class, () -> calculator.calculatePoints(letters),
+				"calculatePoints does not take List<Letter> that contains null values, "
+				+ "should throw IllegalArgumentException.");
 	}
 
 }
